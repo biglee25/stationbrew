e6e77debffd737303912f095c2dcf5b856a97cb4
Lee Attwater <leeattwater@gmail.com>
2020-04-03 12:58:07 +0100

    new

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 function openMenu() {
-    var slideMenu = document.getElementById("menu-wrapper");
-    if (slideMenu.style.display === "none") {
-        slideMenu.style.display = "block";
-    } else {
-        slideMenu.style.display = "none";
+var slideMenu = document.getElementById("menu-wrapper");
+if (slideMenu.style.display === "none") {
+    slideMenu.style.display = "block";
+} else {
+    slideMenu.style.display = "none";
     }
-  }
+}
 
 function closeMenu() {
-    var slideMenu = document.getElementById("menu-wrapper");
-    if (slideMenu.style.display === "block") {
-        slideMenu.style.display = "none";
-    } else {
-        slideMenu.style.display = "block";
+var slideMenu = document.getElementById("menu-wrapper");
+if (slideMenu.style.display === "block") {
+slideMenu.style.display = "none";
+} else {
+slideMenu.style.display = "block";
 
-    }
-  }
+}
+}
 
 
